7326bb4f38e0a1b661609f4f145a9d32d514f538
ramya344 <ramya070.guggilapu@gmail.com>
2017-10-02 00:12:11 +0530

    sample test

---
+++ src/test/java/com/git/GitRepo/Test1.java
@@ -0,0 +1,11 @@
+package com.git.GitRepo;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+  @Test
+  public void launch() {
+	  System.setProperty("webdriver.chrome.driver","D:\\git\\ChromeDriver.exe");
+	  
+  }
+}
