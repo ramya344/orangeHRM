2ae444b231b31d3099d81124e01aa3678cc1113e
ramya344 <ramya070.guggilapu@gmail.com>
2017-10-02 12:55:29 +0530

    code added

--- src/test/java/com/git/GitRepo/Test1.java
+++ src/test/java/com/git/GitRepo/Test1.java
@@ -1,11 +1,15 @@
 package com.git.GitRepo;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Test1 {
   @Test
   public void launch() {
-	  System.setProperty("webdriver.chrome.driver","D:\\git\\ChromeDriver.exe");
+	  System.setProperty("webdriver.chrome.driver","D:\\amma\\ChromeDriver.exe");
+	  WebDriver driver=new ChromeDriver();
+	  driver.get("https://www.google.com");
 	  
   }
 }
